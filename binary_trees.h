5d3a67d0daab01d98550b617da641bb801736b1e
voltamonkey <9461@holbertonstudents.com>
2024-12-13 09:21:16 -0400

    Chack

--- binary_trees.h
+++ binary_trees.h
@@ -30,9 +30,8 @@ typedef struct binary_tree_s binary_tree_t;
 
 /* ======= PROTOTYPES ======= */
 
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
