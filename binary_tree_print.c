790203f4cf193e59ee9364a3a69e57b88e4a7514
voltamonkey <9461@holbertonstudents.com>
2024-12-13 16:04:54 -0400

    bin

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -50,13 +50,13 @@ static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
  *
  * Return: The height of the tree starting at @node
  */
-static size_t _height(const binary_tree_t *tree)
+static size_t height(const binary_tree_t *tree)
 {
 	size_t height_l;
 	size_t height_r;
 
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
+	height_l = tree->left ? 1 + height(tree->left) : 0;
+	height_r = tree->right ? 1 + height(tree->right) : 0;
 	return (height_l > height_r ? height_l : height_r);
 }
 
@@ -72,7 +72,7 @@ void binary_tree_print(const binary_tree_t *tree)
 
 	if (!tree)
 		return;
-	height = _height(tree);
+	height = height(tree);
 	s = malloc(sizeof(*s) * (height + 1));
 	if (!s)
 		return;
