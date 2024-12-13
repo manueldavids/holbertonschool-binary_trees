fdb67e94cc47a585809a3c91f949c539d8680730
voltamonkey <9461@holbertonstudents.com>
2024-12-13 11:16:53 -0400

    0-BinaryUp

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = NULL;
 
 	/* Allocate memory for the new node */
 	new_node = malloc(sizeof(binary_tree_t));
@@ -19,8 +19,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 
 	/* Initialize the node's fields */
-	new_node->n = value;
 	new_node->parent = parent;
+	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
